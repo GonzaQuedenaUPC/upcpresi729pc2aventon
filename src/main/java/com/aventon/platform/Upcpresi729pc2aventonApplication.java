220dbfebc76ebcfd3c82dd1c2133c90b82a210a5
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-07 11:18:30 -0500

    chore: project setup.

---
+++ src/main/java/com/aventon/platform/Upcpresi729pc2aventonApplication.java
@@ -0,0 +1,15 @@
+package com.aventon.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class Upcpresi729pc2aventonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Upcpresi729pc2aventonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aventon/platform/Upcpresi729pc2aventonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aventon.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Upcpresi729pc2aventonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
